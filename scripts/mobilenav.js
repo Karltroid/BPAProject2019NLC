d66c5e265b7b9a21306e097027cf5970e21088c4
Karl Palmer <KarltroidContact@Gmail.com>
2019-03-23 18:18:00 -0400

    Fixed Mobile Nav Menu Bugs & Optimized It's Script

--- scripts/mobilenav.js
+++ scripts/mobilenav.js
@@ -1,31 +1,33 @@
-openSpeed = 1;
-closeSpeed = 2;
+var openSpeed = 1;
+var closeSpeed = 2;
+var navitems = ["mobile-nav-header", "Home", "Maps", "Places", "Food", "Transit", "Attire"];
 
 function opennav()
 {
+	document.getElementsByTagName("body")[0].style.overflow = "hidden";
 	document.getElementById("mobile-nav-items").style.display = "block";
-	document.getElementById("mobile-nav-header").style.animation = "slide-in " + openSpeed * 0.4 + "s";
-	document.getElementById("Home").style.animation = "slide-in " + openSpeed * 0.5 + "s";
-	document.getElementById("Maps").style.animation = "slide-in " + openSpeed * 0.6 + "s";
-	document.getElementById("Places").style.animation = "slide-in " + openSpeed * 0.7 + "s";
-	document.getElementById("Food").style.animation = "slide-in " + openSpeed * 0.8 + "s";
-	document.getElementById("Transit").style.animation = "slide-in " + openSpeed * 0.9 + "s";
-	document.getElementById("Attire").style.animation = "slide-in " + openSpeed * 1 + "s";
-
-
+	var speed = 0.4;
+	for (i = 0; i < navitems.length; i++)
+	{ 
+		document.getElementById(navitems[i]).style.animation = "slide-in " + openSpeed * speed + "s";
+		console.log("navitems[" + i + "].style.animation = 'slide-in' + " + openSpeed + " * " + speed + " + 's'");
+		speed += 0.1;
+	}
 }
 
 function closenav()
 {
+	document.getElementsByTagName("body")[0].style.overflow = "auto";
 	document.getElementById("mobile-nav-header").style.animation = "slide-out " + closeSpeed * 1 + "s";
-	document.getElementById("Home").style.animation = "slide-out " + closeSpeed * 0.9 + "s";
-	document.getElementById("Maps").style.animation = "slide-out " + closeSpeed * 0.8 + "s";
-	document.getElementById("Places").style.animation = "slide-out " + closeSpeed * 0.7 + "s";
-	document.getElementById("Transit").style.animation = "slide-out " + closeSpeed * 0.6 + "s";
-	document.getElementById("Attire").style.animation = "slide-out " + closeSpeed * 0.5 + "s";
-	document.getElementById("Food").style.animation = "slide-out " + closeSpeed * 0.4 + "s";
+	var speed = 1;
+	for (i = 0; i < navitems.length; i++)
+	{ 
+		document.getElementById(navitems[i]).style.animation = "slide-out " + closeSpeed * speed + "s";
+		console.log("navitems[" + i + "].style.animation = 'slide-out' + " + closeSpeed + " * " + speed + " + 's'");
+		speed -= 0.1;
+	}
 
-	setTimeout(hide, ((closeSpeed * .4) * 1000));
+	setTimeout(hide, ((closeSpeed * .1) * 1000));
 }
 
 function hide()
